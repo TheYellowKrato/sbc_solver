7dfccc95759f51cdda4a38ef7dae7a5636b6141c
Maxime <maxime.desmarchelier@gmail.com>
2018-10-07 18:31:58 +0200

    Fix rating calculation

--- Player.py
+++ Player.py
@@ -2,7 +2,7 @@ class Player:
 
     def __init__(self):
         self.resource_id = -1
-        self.rating = -1
+        self.rating = 0
         self.preferred_position = "default"
         self.club_id = -1
         self.nationality_id = -1
